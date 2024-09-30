5ed4b2310a611fd14678cdefd16b701f73212f2a
bluedragonplayz2 <bluedragonplayz2@gmail.com>
2024-09-30 20:56:17 +0800

    v0.0.19-dev added main page wallpaper

--- components/headerBar.vue
+++ components/headerBar.vue
@@ -12,9 +12,9 @@ const links = [{
 </script>
 
 <template>
-  <div class="w-screen flex flex-row justify-center">
+  <div class="w-screen flex flex-row justify-center fixed bg-primary bg-opacity-60">
     <div class=" flex flex-row w-screen   border-b border-gray-200 dark:border-gray-800">
-      <UButton class="flex flex-row" color="transparent">
+      <UButton class="flex flex-row" :ui="{ rounded: 'rounded-none',  variant:{nothing: 'text-{color}-700 dark:text-{color}-200 disabled:bg-transparent dark:disabled:bg-transparent focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-{color}-500 dark:focus-visible:ring-{color}-400'}}" color="gray" variant="nothing">
         <NuxtImg src="/image/test.png" width="70"/>
         <div style="font-family: Kode Mono, sans-serif; font-size: 2.5rem; ">
           BDP.blue
---
+++ components/landingImage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const img = useImage()
+</script>
+
+<template>
+  <div class="w-lvw h-lvh overflow-clip " id="image-container">
+    <NuxtImg
+      src= "/image/placeholders/nebular.webp"
+      fit="outside"
+    />
+  </div>
+</template>
+
+<style scoped>
+.foggy{
+  backdrop-filter: blur(10px);
+}
+</style>
