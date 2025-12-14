1cf77353ee2ece77423af2452d3da3372d8c85cc
bluedragonplayz2 <bluedragonplayz2@gmail.com>
2025-12-14 15:23:49 +0800

    v0.0.22-dev Added webfinger for auth

---
+++ pages/support.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import NotificationBar from "~/components/notificationBar.vue";
+</script>
+
+<template>
+  <header-bar/>
+  <notification-bar/>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
